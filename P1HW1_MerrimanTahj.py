f859fe5ef5b1f99c1482ed80fb7252fc6115e592
MerrimaT <98863503+MerrimaT@users.noreply.github.com>
2022-02-03 22:06:00 -0500

    Add files via upload
    first Py. homework code

---
+++ P1HW1_MerrimanTahj.py
@@ -0,0 +1,13 @@
+
+userNum1= int(input('Enter first number: '))
+print()
+userNum2= int(input('Enter second number: '))
+
+
+TwoNumbersAdd=userNum1 + userNum2 #adding the two input numbers
+twoNumbersMul=userNum1 * userNum2 #mulitply the two input numbers
+
+print(userNum1)
+print(userNum2)
+print(TwoNumbersAdd)
+print(twoNumbersMul)
