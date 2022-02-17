df19eb2ecc82442787f8352fcbe82d89434cf4a4
MerrimaT <98863503+MerrimaT@users.noreply.github.com>
2022-02-17 10:54:23 -0500

    Add files via upload
    mod 4 lab 2

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-user_int = int(input('Enter integer (32 - 126):\n'))
+current_price = int(input())
+last_months_price = int(input())
 
-# FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-user_float = float(input('Enter float:\n')) 
-user_char = (input('Enter character:\n'))
-user_string = (input('Enter string:\n'))
-#desired output: 99 3.77 z Howdy
-print(user_int,user_float,user_char,user_string)
+change_price = current_price - last_months_price
+month_morg = (current_price * 0.051) / 12
+month_morg_p = f'{month_morg:.2f}'
 
-# FIXME (2): Output the four values in reverse
-print(user_string,user_char,user_float,user_int)
-  
-# FIXME (3): Convert the integer to a characer, and output that character
-user_int_char = chr(user_int)
-print(user_int,'converted to a character is',user_int_char)
+current_price_str = chr(36) + str(current_price) + '.'
+change_price_str = chr(36) + str(change_price)
+month_morg_str = chr(36) + str(month_morg_p) +'.'
+
+print('This house is', current_price_str, 'The change is', change_price_str,'since last month.')
+print('The estimated monthly mortgage is', month_morg_str)
+
+#This house is $200000. The change is $-10000 since last month.
+#The estimated monthly mortgage is $850.00.
